d3bfc05ce085bbb9176f8a0649dedc0a7b0492b6
jssdocente <jssgarcia.docente@gmail.com>
2021-02-28 19:12:47 +0100

    [ADD] IMPLEMENTACIÓN SEGURIDAD. Paso 1/N
    * Implementando UserDetails en UsuarioVO
    * Incluir roles como Enumerado (UserRole) y campo Roles como ElementoCollection.
    * Autorities (permisos) se devuelven los Roles en formato "ROLE_USER" o "ROLE_ADMIN", ...

---
+++ src/main/java/com/iesvi/gestionUsuario/domain/UserRole.java
@@ -0,0 +1,5 @@
+package com.iesvi.gestionUsuario.domain;
+
+public enum UserRole {
+    USER, ADMIN
+}
--- src/main/java/com/iesvi/gestionUsuario/domain/UsuarioVO.java
+++ src/main/java/com/iesvi/gestionUsuario/domain/UsuarioVO.java
@@ -3,9 +3,15 @@ package com.iesvi.gestionUsuario.domain;
 import com.iesvi.shared.domain.audit.AuditableEntity;
 import lombok.*;
 import lombok.experimental.WithBy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 @With //TODO:LOMBOK NEW ANNOTATION @WITH
@@ -15,7 +21,9 @@ import java.io.Serializable;
 @EqualsAndHashCode(callSuper = false)
 @Entity(name = "Usuario")
 @Inheritance(strategy = InheritanceType.JOINED)
-public class UsuarioVO extends AuditableEntity implements Serializable {
+public class UsuarioVO extends AuditableEntity implements UserDetails, Serializable {
+
+    private static final long serialVersionUID = -7863998719466555120L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,9 +38,48 @@ public class UsuarioVO extends AuditableEntity implements Serializable {
     @Column(length = 150, nullable = false)
     String nombre;
 
+    @ElementCollection(fetch = FetchType.EAGER)  //Indica una colección de tipos-simples, no de entidades.
+    @Enumerated(EnumType.STRING)
+    Set<UserRole> roles;
+
     public UsuarioVO(String nombre, String nombre_usuario, String password) {
         this.nombre_usuario = nombre_usuario;
         this.password = password;
         this.nombre = nombre;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(ur -> new SimpleGrantedAuthority("ROLE_" + ur.name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return this.getNombre_usuario();
+    }
+
+    //No gestionamos expiraciones de cuenta
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    //No gestionamos cuentas bloqueadas
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    //No gestionamos credenciales expiradas
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
